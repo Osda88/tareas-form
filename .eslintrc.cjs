6672d060187b392acab6166b66979994b71be789
Osda88 <osda88_1@hotmail.com>
2024-03-14 22:19:33 -0600

    arreglo de eslin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'no-unused-vars':'["error", { "vars": "local" }]',
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
